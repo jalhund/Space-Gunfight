1aad928a14bae0b476214d072295618c796dbaf2
JAlHund <JAlHund@users.noreply.github.com>
2020-07-18 11:11:22 +0300

    Added background, players and control

---
+++ game/game_objects/weapon.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "weapon"
+  type: "sprite"
+  data: "tile_set: \"/atlas/game.atlas\"\n"
+  "default_animation: \"weapon\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 2.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
